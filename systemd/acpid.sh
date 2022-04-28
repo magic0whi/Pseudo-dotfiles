fa3ca92c5b8ecb8bb4710636188a08908de9c9da
Proteus <lollipop.studio.cn@gmail.com>
2022-04-28 22:06:41 +0800

    Old battery drain faster, make hibernate active below 15%

--- systemd/acpid.sh
+++ systemd/acpid.sh
@@ -2,7 +2,8 @@
 /usr/bin/acpi -b | /usr/bin/awk -F'[,:%]' '{if ($3!=0) print $2, $3}' | {
     read -r status capacity
 
-    if [ "$status" = Discharging -a "$capacity" -lt 10 ]; then
+    if [ "${status}" = 'Discharging' -a "${capacity}" -lt 15 ]
+    then
         /usr/bin/logger "Critical battery threshold"
         /usr/bin/systemctl hibernate
     fi
