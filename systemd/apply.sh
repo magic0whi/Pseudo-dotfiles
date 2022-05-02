b6c316bd3db411619dc11d8bf01019a428bda324
Proteus <lollipop.studio.cn@gmail.com>
2022-05-02 17:36:26 +0800

    Reset Surface Bluetooth adapter after hibernate

--- systemd/apply.sh
+++ systemd/apply.sh
@@ -2,4 +2,7 @@
 
 cp acpid.timer /etc/systemd/system/
 cp acpid.service /etc/systemd/system/
-cp acpid.sh /usr/local/bin/
+cp acpid /usr/local/bin/
+
+cp surface-hibernate-pre.service /etc/systemd/system/
+cp surface-hibernate-post.service /etc/systemd/system/
