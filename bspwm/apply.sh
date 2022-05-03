6e300edd8fad76a68645ba98053670e00132fe46
Proteus <lollipop.studio.cn@gmail.com>
2022-05-03 13:00:21 +0800

    Bspwm-sxhkd: Using systemctl to do restart/reload operation

--- bspwm/apply.sh
+++ bspwm/apply.sh
@@ -2,6 +2,6 @@
 
 cp bspwmrc ~/.config/bspwm/bspwmrc
 cp sxhkdrc ~/.config/sxhkd/sxhkdrc
-cp panel        /usr/local/bin/
-cp panel_bar    /usr/local/bin/
-cp panel_colors /usr/local/bin/
+sudo cp panel        /usr/local/bin/
+sudo cp panel_bar    /usr/local/bin/
+sudo cp panel_colors /usr/local/bin/
