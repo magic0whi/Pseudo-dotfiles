d9fa3869579da20edfa8285e4159bbc996c1521c
proteus <lollipop.studio.cn@gmail.com>
2022-10-31 19:36:12 +0800

    Improve these simple apply & health check scripts

--- bspwm/apply.sh
+++ bspwm/apply.sh
@@ -1,13 +1,18 @@
 #!/bin/sh
 
-[ -d ~/.config/bspwm ] || mkdir ~/.config/bspwm
-[ -d ~/.config/sxhkd ] || mkdir ~/.config/sxhkd
-cp bspwmrc ~/.config/bspwm/
-cp sxhkdrc ~/.config/sxhkd/
+a1=('bspwmrc' 'sxhkdrc' 'redshift.conf' 'stalonetrayrc' 'picom.conf' 'polybar.ini:config.ini')
+a2=("${HOME}/.config/bspwm" "${HOME}/.config/sxhkd" "${HOME}/.config/redshift" "${HOME}/.config" "${HOME}/.config/picom" "${HOME}/.config/polybar")
 
-[ -d ~/.config/redshift ] || mkdir ~/.config/redshift
-cp redshift.conf ~/.config/redshift/
 
-cp stalonetrayrc ~/.config/
-cp picom.conf ~/.config/picom/
-cp polybar.ini ~/.config/polybar/config.ini
+for ((i=0; i<${#a1[@]}; i++))
+do
+    if [ ! -d ${a2[$i]} ]; then
+        echo Create directory ${a2[$i]}
+        mkdir -p ${a2[$i]}
+        echo Copy file ${a1[$i]%:*} to "${a2[$i]}/${a1[$i]#*:}"
+        cp ${a1[$i]%:*} "${a2[$i]}/${a1[$i]#*:}"
+    elif [[ $(sha256sum "${a1[$i]%:*}" | awk '{print $1}') != $(sha256sum "${a2[$i]}/${a1[$i]#*:}" | awk '{print $1}') ]]; then
+        echo Update file ${a1[$i]%:*} to "${a2[$i]}/${a1[$i]#*:}"
+        cp ${a1[$i]%:*} "${a2[$i]}/${a1[$i]#*:}"
+    fi
+done
