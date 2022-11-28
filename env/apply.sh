1277d63b3a1b0a854517b00bf12f0354e8f1e2aa
proteus <lollipop.studio.cn@gmail.com>
2022-11-28 20:01:52 +0800

    Some updates

--- env/apply.sh
+++ env/apply.sh
@@ -3,3 +3,4 @@
  [ -d ~/.config/environment.d ] || mkdir ~/.config/environment.d
 
  cp fcitx5.conf ~/.config/environment.d/
+ cp howdy.conf ~/.config/environment.d/
--- systemd/x11nuc/apply.sh
+++ systemd/x11nuc/apply.sh
@@ -4,3 +4,4 @@ sudo cp acpid.timer /etc/systemd/system/
 sudo cp acpid.service /etc/systemd/system/
 sudo cp acpid /usr/local/bin/
 sudo cp systemd-networkd/* /etc/systemd/network/
+sudo cp ly.conf /etc/systemd/system/ly.service.d/override.conf
