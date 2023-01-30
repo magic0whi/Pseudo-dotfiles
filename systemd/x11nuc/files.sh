718dd3880ccc55192bb576c84606076cdcff6021
proteus <lollipop.studio.cn@gmail.com>
2023-01-30 21:31:39 +0800

    systemd-networkd: Wait for any interface to become online rather than all interfaces

--- systemd/x11nuc/files.sh
+++ systemd/x11nuc/files.sh
@@ -1,3 +1,3 @@
 #/bin/sh
-a1=('iwd.conf:main.conf' 'systemd-networkd-20-macvtap0.netdev:20-macvtap0.netdev' 'systemd-networkd-20-macvtap0.network:20-macvtap0.network' 'systemd-networkd-20-wired.network:20-wired.network' 'systemd-networkd-25-wireless.network:25-wireless.network' 'resolved.conf.d/dns_over_tls.conf:dns_over_tls.conf' 'resolved.conf.d/dnssec.conf:dnssec.conf')
-a2=('/etc/iwd' '/etc/systemd/network' '/etc/systemd/network' '/etc/systemd/network' '/etc/systemd/network' '/etc/systemd/resolved.conf.d' '/etc/systemd/resolved.conf.d')
+a1=('iwd.conf:main.conf' 'systemd-networkd-20-macvtap0.netdev:20-macvtap0.netdev' 'systemd-networkd-20-macvtap0.network:20-macvtap0.network' 'systemd-networkd-20-wired.network:20-wired.network' 'systemd-networkd-25-wireless.network:25-wireless.network' 'resolved.conf.d/dns_over_tls.conf:dns_over_tls.conf' 'resolved.conf.d/dnssec.conf:dnssec.conf' 'systemd-networkd-wait-online.override.conf:override.conf')
+a2=('/etc/iwd' '/etc/systemd/network' '/etc/systemd/network' '/etc/systemd/network' '/etc/systemd/network' '/etc/systemd/resolved.conf.d' '/etc/systemd/resolved.conf.d' '/etc/systemd/system/systemd-networkd-wait-online.service.d')
